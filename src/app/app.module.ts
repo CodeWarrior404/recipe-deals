f77a3d25452fb4731a84ccb2e974d6018fa90016
Pradeep James <pjm123mail-github@yahoo.co.in>
2017-08-24 15:33:25 -0500

    Added recipe paste area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RecipeComponent } from './recipe/recipe.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
 import { DealsComponent } from './deals/deals.component';
+import { InputTextareaModule, PanelModule } from 'primeng/primeng';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { DealsComponent } from './deals/deals.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    PanelModule,
+    InputTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
