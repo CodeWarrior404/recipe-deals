27b22945e48a21652942c35ffcd2d6dcc3f68418
Pradeep James <pjm123mail-github@yahoo.co.in>
2017-08-24 15:09:06 -0500

    Adding component stubs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipeComponent } from './recipe/recipe.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { DealsComponent } from './deals/deals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    IngredientsComponent,
+    DealsComponent
   ],
   imports: [
     BrowserModule,
